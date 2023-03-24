3595c286794fe6e8269673d8c84d019e52d1ebe0
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-24 22:31:22 +0800

    up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_24_133102) do
+ActiveRecord::Schema.define(version: 2023_03_24_142823) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_03_24_133102) do
     t.integer "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
